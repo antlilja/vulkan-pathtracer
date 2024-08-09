a702394371bb1e8cce61569a7997882a1e11ea53
antlilja <liljaanton2001@gmail.com>
2024-08-09 03:34:26 +0200

    Update vulkan-zig and remove custom ShaderCompileStep

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const ShaderCompileStep = @import("ShaderCompileStep.zig");
+const ShaderCompileStep = @import("vulkan-zig").ShaderCompileStep;
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -79,7 +79,7 @@ pub fn build(b: *std.Build) !void {
 
     const shaders = ShaderCompileStep.create(
         b,
-        glslang.getEmittedBin(),
+        .{ .lazy_path = glslang.getEmittedBin() },
         &[_][]const u8{ "-V", "--target-env", "vulkan1.2" },
         "-o",
     );
